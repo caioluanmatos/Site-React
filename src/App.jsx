9eeed967dce104220398c99bee871ef8da096e4b
Caio Luan <157083928+caioluanmatos@users.noreply.github.com>
2026-06-16 13:39:31 -0300

    Primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+export default function App() {
+  return (
+    <h1>Meu primeiro React funcionando 🚀</h1>
+  )
+}
