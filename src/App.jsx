536c8d97c92513bb39212c45ce44a6e4c30e1b78
Caio Luan <157083928+caioluanmatos@users.noreply.github.com>
2026-06-18 00:26:10 -0300

    Login-base-HTML-Icon-

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+
 import './App.css'
+import Login from './components/login/login';
 
 export default function App() {
   return (
-    <><h1>Meu primeiro React funcionando 🚀</h1><p>ola</p></>
+    <div className='app'>
+          < Login />
+    </div>
   )
 }
